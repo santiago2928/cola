be4fd54204b042316773c4091586937287d8ee4d
santiago2928 <149178947+santiago2928@users.noreply.github.com>
2024-02-14 19:54:57 -0500

    Initial commit

---
+++ src/main/java/cola/Base.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cola;
+
+/**
+ *
+ * @author santiago.osorio_gra
+ */
+public abstract class Base {
+    
+    public abstract Base copy(); 
+}
---
+++ src/main/java/cola/Cola.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cola;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author santiago.osorio_gra
+ */
+public class Cola <T extends Base> {
+    
+     private LinkedList<T> elementos; 
+    
+     public Cola(){
+         elementos = new LinkedList<>(); 
+     }
+
+     public void encolar(T elemento){
+         elementos.addLast(elemento);
+     }
+     
+     public T desencolar(){
+         return elementos.removeFirst(); 
+     }
+     
+     public boolean estaVacia(){
+        return elementos.isEmpty();
+     }
+    
+}
